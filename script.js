c9ff9109ee4d3c9aa70081332ad503f58bf04044
Oluwafemi Olukoya <44469556+Oluwafemi21@users.noreply.github.com>
2021-12-30 00:09:22 +0000

    added an error message

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 const list = document.getElementById('list');
 const btn = document.getElementById('btn');
 const listInput = document.getElementById('list-input'); 
+const errorMsg = document.querySelector('.error');
 
 btn.addEventListener('click', () => {
     let i = listInput.value;
-    list.innerHTML += `<li>${i}</li>`;
+    if(i === ''){
+      errorMsg.classList.remove('hidden');
+    } else{
+      errorMsg.classList.add('hidden');
+      list.innerHTML += `<li>${i}</li>`;
 
-    let items = document.querySelectorAll('li');
+      let items = document.querySelectorAll('li');
 
-    items.forEach(l => {
+      items.forEach(l => {
         l.addEventListener('click',() => {
             l.classList.toggle('line-through')
         });
         l.addEventListener('dblclick',()=>{
             l.classList.toggle('hidden');
         })
-    })
+      })
+    }
+    
 })
